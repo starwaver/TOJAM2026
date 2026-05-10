c73ed809ad3fd650b59c9331e5a99bd4eedf5a55
starwaver <jackjack_liu@hotmail.com>
2026-05-10 02:10:03 -0400

    Added coffee making mini game

--- src/game/data/TaskRegistry.ts
+++ src/game/data/TaskRegistry.ts
@@ -18,4 +18,12 @@ export const TaskRegistry: TaskDefinition[] = [
     minDifficulty: 1,
     maxDifficulty: 10,
   },
+  {
+    id: 'coffee_run',
+    displayName: 'Boss Coffee Run',
+    scene: SceneKeys.coffeeRun,
+    baseTimeLimit: 30,
+    minDifficulty: 1,
+    maxDifficulty: 10,
+  },
 ];
